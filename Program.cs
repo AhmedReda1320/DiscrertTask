8aa40981562370b3c87ef13f992079aeb87e1a64
AhmedReda1320 <121174941+AhmedReda1320@users.noreply.github.com>
2022-12-21 16:07:09 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,67 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prime_Number
+namespace Perefect_Number
 {
     internal class Program
     {
+        static bool isnumber(int number)
+        {
+            int sum = 0;
+            for (int s = 1; s < number; s++)
+            {
+                if (number % s== 0) sum = sum + s;
+            }
+            if (sum == number) return true;
+            return false;
+
+        }
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello");
-            //هنطلب من البوزر انه يدخل الرقم الاول 
-            Console.WriteLine("Please enter the first number:");
-            // هنا هنخزن القيمه اللي اليوزر دخلها في متغير int
-            int first = int.Parse(Console.ReadLine());
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine(" please enter start number: ");
+                int i = int.Parse(Console.ReadLine());
 
-            //وهنا الرقم الثاني 
-            Console.WriteLine("Please enter the second number:");
-            // وهنا هناخدها ونخزنها في متغيرint برضوا
-            int second = int.Parse(Console.ReadLine());
-            // هنا هنبدا نطبع 
-            Console.WriteLine("The Prime Numbers");
-            //هنستخدم التكرار بي for
+                Console.WriteLine("please enter end number: ");
+                int v = int.Parse(Console.ReadLine());
 
-            for (int a=first; a<=second; a++)  //دي التكرارات اللي بين الارقام اللي اليوزر دخلهم
-            {// هنا هتبدا التكرارات بتاعت الfor loop
-                // من اول هنا هيبدا البرنامج بفحص الارقام اللي اليوزر دخلهم واللي بينهم وهيبدا بطباعه الارقام الاوليه فقط
-                bool IsPrimeNumber = true; 
-                   if (a==2 || a == 3)
-                   {
-                    Console.WriteLine(a);
-                   }
-                   else if(a==1 || a % 2 == 0)
-                   {
-                    continue;
-                   }
-                   else
-                   {
-                      for (int s=3; s<=Math .Sqrt (a); s+=2)
-                      { 
-                        if (a % s == 0)
-                        {
-                            IsPrimeNumber = false;
-                            break;
-                        }
-                      }
-                      if (IsPrimeNumber)
-                      {
-                        Console.WriteLine(a);
-                      }
+                for (int a = i; a <= v; a++)
+                {
+                    if (isnumber(a)) Console.WriteLine("{0},", a);
 
-                   }
-
-
-
-
+                }
+                Console.WriteLine("End");
+               Console.ReadKey ();
             }
-            Console.WriteLine("end");
-            Console.ReadKey();
-
         }
     }
 }
